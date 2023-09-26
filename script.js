88976f5a95ad6e5a70f4efadca2396d4937498de
Abe <abe.wav.bis@gmail.com>
2023-09-25 20:37:05 -0500

    updated js - added validation to ensure all selected character types are included in generated password

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ function generatePassword() {
   var newPassArray = [];
   var newPass = "";
 
+  var lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  var pool = [];
+  var rand = "";
+
   var passCriteria = {
     passLength: prompt("Enter password length (8-128 characters):"),
     containsLower: false,
@@ -25,7 +32,7 @@ function generatePassword() {
   };
 
   //takes user length input & checks if it is within character range.
-  //If it is within range, it will confirm the different character types & set newPassData[0] as the length value.
+  //If it is within range, it will confirm the different character types & set passCriteria.passLength as the length input.
   //If it is outside of the range, returns an alert and stops the function.
   function validate(userinput) {
     if (passCriteria.passLength >= 8 && passCriteria.passLength <= 128) {
@@ -33,7 +40,6 @@ function generatePassword() {
       passValidate();
     } else {
       alert("Invalid password length. Password must be between 8-128 characters.");
-      newPass = "Password generation failed. Please try again with valid inputs.";
       return;
     }
   }
@@ -44,8 +50,6 @@ function generatePassword() {
     passCriteria.containsCaps = confirm("Would you like your password to contain capital letters?");
     passCriteria.containsNum = confirm("Would you like your password to contain numbers?");
     passCriteria.containsSpecial = confirm("Would you like your password to contain special characters?");
-
-    //for testing only, remove when further along;
   };
 
   function passValidate() {
@@ -53,57 +57,41 @@ function generatePassword() {
       generate();
     } else {
       alert("Password must contain at least one type of character. Please try again with valid inputs.");
-      newPass = "Password must contain at least one type of character. Please try again with valid inputs.";
       return;
     }
   };
 
   function generate() {
-    var lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-    var pool = [];
-
-    var rand = "";
 
     function assemblePool(){
       if (passCriteria.containsLower){
         pool = pool.concat(lowers);
-        console.log("Character Pool: " + pool);
       }
 
       if (passCriteria.containsCaps){
         pool = pool.concat(caps);
-        console.log("Character Pool: " + pool);
       }
 
       if (passCriteria.containsNum){
         pool = pool.concat(numbers);
-        console.log("Character Pool: " + pool);
       }
 
       if (passCriteria.containsSpecial){
         pool = pool.concat(special);
-        console.log("Character Pool: " + pool);
       }
+      console.log("Character Pool: " + pool);
     };
-
-    rand = pool[Math.floor(Math.random() * pool.length)];
 
     //Concats relevant character arrays into pool array
     assemblePool();
-    console.log(pool);
 
     //This for loop picks random characters out of the pool array until it meets the user-specified password length
     for (let i = 0; i < passCriteria.passLength; i++) {
       rand = pool[Math.floor(Math.random() * pool.length)];
-      console.log(rand);
       newPassArray.push(...rand);
-      console.log(newPassArray);
     };
 
-    return newPass;
+    console.log("new pass array: " + newPassArray);
   };
 
   //calls validate function using user inputted length
@@ -116,13 +104,36 @@ function generatePassword() {
   console.log("#s: " + passCriteria.containsNum);
   console.log("Special: " + passCriteria.containsSpecial);
 
+  //searches newly generated newPassArray to find at least one of a specific character type: returns true if any are included
+  var lowersFound = newPassArray.some(ai => lowers.includes(ai));
+  var capsFound = newPassArray.some(ai => caps.includes(ai));
+  var numbersFound = newPassArray.some(ai => numbers.includes(ai));
+  var specialFound = newPassArray.some(ai => special.includes(ai));
+
+  //Validates that at least one of each selected character type is included in the newPassArray. Will call generate() until password meets criteria.
+  if (passCriteria.containsLower && !lowersFound) {
+    newPassArray = [];
+    generate();
+  }
+  if (passCriteria.containsCaps && !capsFound) {
+    newPassArray = [];
+    generate();
+  }
+  if (passCriteria.containsNum && !numbersFound) {
+    newPassArray = [];
+    generate();
+  }
+  if (passCriteria.containsSpecial && !specialFound) {
+    newPassArray = [];
+    generate();
+  }
+
   //converts newPassArray content into a string without commas
   newPass = newPassArray.join("");
 
   //returns newly generated password
   return newPass;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
